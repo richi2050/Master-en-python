29d3c643b8fd8c6c377136b6089441420fe18b8e
Ricardo Lugo <ricardo.lugo.recillas@gmail.com>
2021-04-12 23:06:22 -0500

    Multiples vistas y URLs
    esta es la descricion de poner multiples vistas y urls

--- 22-django/aprendiendoDjango/miapp/views.py
+++ 22-django/aprendiendoDjango/miapp/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("""
+        <h1>Inicio</h1>
+    """)
 
 def hola_mundo(request):
     return HttpResponse("Hola mundo con Django!!!!")
+
+def pagina(request):
+    return HttpResponse("""
+    <h1>pagina d emi web</h1>
+    """)
