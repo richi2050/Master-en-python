ad78f92c4167db41dca68ae9a9cd23b713d66003
Ricardo Lugo <ricardo.lugo.recillas@gmail.com>
2021-04-15 13:03:51 -0500

    Ejecutar SQL desde Django
    Ejecutar SQL desde Django

--- 22-django/aprendiendoDjango/miapp/views.py
+++ 22-django/aprendiendoDjango/miapp/views.py
@@ -100,6 +100,7 @@ def articulos(request):
     articulos = Article.objects.filter(id__lt=2) #solo que busca menores que lt
     articulos = Article.objects.filter(id__lte=2) #solo que busca menores he iguales que gt
     articulos = Article.objects.all().exclude(public=True)
+    articulos = Article.objects.raw("SELECT * FROM miapp_article where public=True")
 
     return render(request, 'articulos.html', {
         'articulos': articulos
